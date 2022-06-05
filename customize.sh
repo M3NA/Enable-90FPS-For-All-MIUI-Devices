07e1d0380e3c4d90b4fcaae0ea226f1f21f5daa9
Mena <menamaged150@gmail.com>
2022-06-06 00:53:20 +0200

    ..

--- customize.sh
+++ customize.sh
@@ -1,2 +1,2 @@
-mkdir $MODPATH/test
-cp system/product/etc/device_features/*.xml $MODPATH/test/
+cp system/product/etc/device_features/*.xml $MODPATH/system/product/etc/device_features/
+sed -i  '\: <item>120</item>:a <item>90</item>' $MODPATH/system/product/etc/device_features/*.xml 
