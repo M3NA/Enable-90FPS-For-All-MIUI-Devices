23f0c1c1c116b6bb04b9a8538f120345adb6d0cb
Mena Maged <menamaged150@gmail.com>
2022-06-09 10:20:31 +0200

    FIX

--- customize.sh
+++ customize.sh
@@ -1,2 +1,3 @@
+mkdir -p system/product/etc/device_features/
 cp system/product/etc/device_features/*.xml $MODPATH/system/product/etc/device_features/
 sed -i  '\: <item>120</item>:a <item>90</item>' $MODPATH/system/product/etc/device_features/*.xml 
